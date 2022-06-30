342e1476296f7be3b636369e3e11052856da4565
Nkululeko <nkululekob.maseko@gmail.com>
2022-06-30 20:34:08 +0200

    Using image links from db to populate mansory component

--- components/apiRequest.ts
+++ components/apiRequest.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { unsplashURL } from "../pages";
 import { UnsplashSchema } from "../prisma/unsplash";
 
-export const getAllUnsplash = async (): Promise<UnsplashSchema[] | null> => {
+export const getAllUnsplash = async (): Promise<any[] | null> => {
   if (!unsplashURL) return null;
   const unsplashResponse = await axios.get(unsplashURL);
   return unsplashResponse.data;
